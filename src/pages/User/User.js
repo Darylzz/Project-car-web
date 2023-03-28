ff4966a77b9dab66ae548decc7415ecf6be78f8f
Darylzz <cheer_bo50@hotmail.com>
2023-03-28 15:41:09 +0700

    fetch all car user

--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,13 +1,23 @@
 import "./User.css";
 import useAuth from "../../hook/useAuth";
-import carGTR from "../../asset/GTR-R35.jpeg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Modal/Modal";
+import * as carApi from "../../api/carApi";
 
 export default function User() {
   const { logout } = useAuth();
 
   const [openModal, setOpenModal] = useState(false);
+  const [allUserCar, setAllUserCar] = useState([]);
+
+  useEffect(() => {
+    const fetchUserCar = async () => {
+      const res = await carApi.getAllUserCar();
+      // console.log(res.data.car);
+      setAllUserCar(res.data.car);
+    };
+    fetchUserCar();
+  }, []);
 
   return (
     <>
@@ -22,21 +32,25 @@ export default function User() {
         </div>
       </div>
       <div className="UserContainer">
-        <div className="UserContent">
-          <div className="UserFlex">
-            <div className="UserCarImage">
-              <img src={carGTR} alt="" />
-            </div>
-          </div>
-          <div className="UserCarContent">
+        {allUserCar?.map(el => (
+          <div className="UserContent" key={el.id}>
             <div className="UserFlex">
-              <div className="UserCarInfo">
-                <h3>Name: GTR R35 Nismo</h3>
-                <button>กดเพื่ออ่านข้อมูล</button>
+              <div className="UserCarImage">
+                <img src={process.env.REACT_APP_URL + el.image} alt="" />
+              </div>
+            </div>
+            <div className="UserCarContent">
+              <div className="UserFlex">
+                <div className="UserCarInfo">
+                  <h3>
+                    Name: {el.brand} {el.model}
+                  </h3>
+                  <button>กดเพื่อแก้ไขข้อมูล</button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </>
   );
